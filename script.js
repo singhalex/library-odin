6b38b26f4e9000f0f497c01827ff4e2044bbc1e9
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-12-21 13:15:07 -0800

    Refactor code and add comments

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
+const libraryContainer = document.getElementById('library-container');
 const library = [];
 
 function Book(title, author, pages) {
-  // Creates a new book
+  // Book constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = false;
 }
-
-function addBookToLibrary() {
-  // do stuff here
-}
-
-const book1 = new Book('The Grinch', 'Dr. Suess', '50');
-const book2 = new Book('The Very Hungry Caterpillar', 'Eric Carle', '30');
-const book3 = new Book('The Bible', 'Jesus', '1000000');
-
-library[0] = book1;
-library[1] = book2;
-library[2] = book3;
-
-const libraryContainer = document.getElementById('library-container');
-
-library.forEach((book) => displayBooks(book));
-
 function displayBooks(book) {
-  // Creates a card for each book and appends it to the library container
-
+  // Creates the various elements the comprise a card
+  const bookCard = document.createElement('div');
   const title = document.createElement('p');
   const author = document.createElement('p');
   const pages = document.createElement('p');
-  const bookCard = document.createElement('div');
   const deleteBook = document.createElement('button');
   const haveRead = document.createElement('input');
+
+  // Adds the appropriate classes and text to the elements
+  bookCard.className = 'book-card';
 
   title.innerText = book.title;
   title.className = 'title';
@@ -49,8 +35,7 @@ function displayBooks(book) {
   haveRead.setAttribute('type', 'checkbox');
   haveRead.className = 'have-read';
 
-  bookCard.className = 'book-card';
-
+  // Builds the cards structure and adds it to the DOM
   libraryContainer.appendChild(bookCard);
   bookCard.appendChild(title);
   bookCard.appendChild(author);
@@ -58,3 +43,24 @@ function displayBooks(book) {
   bookCard.appendChild(deleteBook);
   bookCard.appendChild(haveRead);
 }
+
+function addBookToLibrary() {
+  // Adds each book in the library to the page
+  library.forEach((book) => displayBooks(book));
+}
+
+function createNewBook() {
+  // Take user input to form a new book object and add it to the library array
+}
+
+// Test book objects
+const book1 = new Book('The Grinch', 'Dr. Suess', '50');
+const book2 = new Book('The Very Hungry Caterpillar', 'Eric Carle', '30');
+const book3 = new Book('The Bible', 'Jesus', '1000000');
+
+// Add test books to the library
+library[0] = book1;
+library[1] = book2;
+library[2] = book3;
+
+addBookToLibrary();
